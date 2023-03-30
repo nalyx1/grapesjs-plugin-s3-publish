168c07aeea7f8ddb0bbe653b7d9437e0134c2f3a
João Rodrigues <joaorodriguez0106@gmail.com>
2023-03-30 16:18:27 -0300

    fix: remove unused variables and change axios url

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import AWS from "aws-sdk";
 import axios from "axios";
 
 export default (editor, opts = {}) => {
-  const { accessKeyId, secretAccessKey, bucketName, id } = opts;
+  const { domain, id, action } = opts;
   const panelManager = editor.Panels;
   const htmlContentType = "text/html";
   const cssContentType = "text/css";
@@ -14,15 +13,6 @@ export default (editor, opts = {}) => {
   const preCss = "";
   const postCss = "";
 
-  AWS.config.update({
-    accessKeyId: accessKeyId,
-    secretAccessKey: secretAccessKey,
-  });
-
-  if (![accessKeyId, secretAccessKey].every(Boolean)) {
-    throw new Error("Configuração do AWS está faltando");
-  }
-
   const newButton = panelManager.addButton("options", {
     id: btnText,
     className: icon,
@@ -32,73 +22,33 @@ export default (editor, opts = {}) => {
   });
 
   editor.Commands.add("publishToS3", {
-    run: async (editor, sender) => {
+    run: async (editor) => {
       const htmlFileBody = `${preHtml}${editor.getHtml()}${postHtml}`;
       const styleFileBody = `${preCss}${editor.getCss()}${postCss}`;
 
-      const { data } = await axios.post(`http://localhost:8056/lp-builder/deploy/s3`, {
-        bucketName: bucketName,
+      await axios.post(`https://gestao.abare.cloud/lp-builder/deploy/s3`, {
+        action,
+        domain,
         lp: {
-          "id": id,
-          "content": [
-          {
-            body: {
-              Body: htmlFileBody,
-              Key: "index.html",
-              ContentType: htmlContentType,
+          id: id,
+          content: [
+            {
+              body: {
+                Key: "index.html",
+                Body: htmlFileBody,
+                ContentType: htmlContentType,
+              },
             },
-          },
-          {
-            body: {
-              Body: styleFileBody,
-              Key: "style.css",
-              ContentType: cssContentType,
+            {
+              body: {
+                Key: "style.css",
+                Body: styleFileBody,
+                ContentType: cssContentType,
+              },
             },
-          },
-        ]},
+          ],
+        },
       });
-
-      console.log(data);
-      // console.log(styleFileBody);
-
-      // const s3 = new AWS.S3();
-
-      // s3.createBucket(
-      //   { Bucket: "lp-tonelli-sanja-duisahduysdsahu" },
-      //   (err, data) => {
-      //     if (err) {
-      //       console.error(err);
-      //     } else {
-      //       console.log(data);
-      //     }
-      //   }
-      // );
-
-      // const s3PublishPromises = [
-      //   { body: htmlFileBody, key: "index.html", mimeType: htmlContentType },
-      //   { body: styleFileBody, key: "style.css", mimeType: cssContentType },
-      // ].map(
-      //   (file) =>
-      //     new Promise((resolve, reject) => {
-      //       const s3Params = {
-      //         Bucket: "lp-tonelli-sanja-duisahduysdsahu",
-      //         Body: file.body,
-      //         Key: `${prefix}/${file.key}`,
-      //         ContentType: file.mimeType,
-      //       };
-      //       s3.putObject(s3Params, (err, data) => {
-      //         if (err) {
-      //           console.error(err);
-      //           reject(err);
-      //         } else {
-      //           console.log(data);
-      //           resolve(data);
-      //         }
-      //       });
-      //     })
-      // );
-
-      await Promise.all(s3PublishPromises);
       const command = editor.Commands.get("publishToS3");
       command.stop();
     },
